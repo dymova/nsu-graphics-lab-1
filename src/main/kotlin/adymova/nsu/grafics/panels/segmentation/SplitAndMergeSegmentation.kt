af9d28377c7536f53a0b0c4b1a941f1567f6fbc6
nastya <adImova05@gmail.com>
2018-03-25 19:32:13 +0700

    find neighbors phase for splitAndMergeSegmentation

--- src/main/kotlin/adymova/nsu/grafics/panels/segmentation/SplitAndMergeSegmentation.kt
+++ src/main/kotlin/adymova/nsu/grafics/panels/segmentation/SplitAndMergeSegmentation.kt
@@ -22,8 +22,6 @@ class SplitAndMergeSegmentation {
             fillRegionsMap(it, regionsMap, width, currentRegionId)
             currentRegionId++
         }
-        println()
-
     }
 
     private fun fillRegionsMap(it: Node, regionsMap: IntArray, width: Int, currentRegionId: Int) {
@@ -41,16 +39,20 @@ class Node(
         private val yStart: Int,
         val xEnd: Int,
         val yEnd: Int,
-        val imageAsLabArrays: ImageAsLabArrays,
+        private val imageAsLabArrays: ImageAsLabArrays,
         val parent: Node?
 ) {
     var passedByMergeAlgorithm = false
-    var childArray: Array<Node> = emptyArray()
+    var children: Array<Node> = emptyArray()
+    private val leftTopChildIndex = 0
+    private val rightTopChildIndex = 1
+    private val leftBottomChildIndex = 2
+    private val rightBottomChildIndex = 3
 
 
     fun splitIfPossible(maxDifference: Double, metrics: (Lab, Lab) -> Double) {
         if (!isHomogeneous(maxDifference, metrics) && xEnd != xStart - 1 || yEnd == yStart - 1) {
-            childArray = split()
+            children = split()
         }
 
     }
@@ -86,7 +88,7 @@ class Node(
 
     fun pass(action: (Node) -> Unit) {
         action(this)
-        for (child in childArray) {
+        for (child in children) {
             child.pass(action)
         }
     }
@@ -100,22 +102,119 @@ class Node(
     }
 
     private fun isLeaf(): Boolean {
-        return childArray.isEmpty()
+        return children.isEmpty()
+    }
+
+    fun findNeigbors(): Neighbors {
+        val neighbors = Neighbors()
+        var parent = parent ?: return neighbors
+        var currentNode = this
+
+
+        while (true) {
+            when (currentNode) {
+                parent.children[leftTopChildIndex] -> {
+                    if (neighbors.right.isEmpty()) {
+                        parent.children[rightTopChildIndex].collectLeftSideChildren(neighbors.right)
+                    }
+                    if (neighbors.bottom.isEmpty()) {
+                        parent.children[leftBottomChildIndex].collectTopSideChildren(neighbors.bottom)
+                    }
+                }
+                parent.children[rightTopChildIndex] -> {
+                    if (neighbors.left.isEmpty()) {
+                        parent.children[leftTopChildIndex].collectRightSideChildren(neighbors.left)
+                    }
+                    if (neighbors.bottom.isEmpty()) {
+                        parent.children[rightBottomChildIndex].collectTopSideChildren(neighbors.bottom)
+                    }
+                }
+                parent.children[leftBottomChildIndex] -> {
+                    if (neighbors.top.isEmpty()) {
+                        parent.children[leftTopChildIndex].collectBottomSideChildren(neighbors.top)
+                    }
+                    if (neighbors.right.isEmpty()) {
+                        parent.children[rightBottomChildIndex].collectLeftSideChildren(neighbors.right)
+                    }
+                }
+                parent.children[rightBottomChildIndex] -> {
+                    if (neighbors.top.isEmpty()) {
+                        parent.children[rightTopChildIndex].collectBottomSideChildren(neighbors.top)
+                    }
+                    if (neighbors.left.isEmpty()) {
+                        parent.children[leftBottomChildIndex].collectRightSideChildren(neighbors.left)
+                    }
+                }
+            }
+
+            if ((parent.xStart == 0 || neighbors.left.isNotEmpty()) &&
+                    (parent.yStart == 0 || neighbors.top.isNotEmpty()) &&
+                    (parent.xEnd == imageAsLabArrays.width || neighbors.right.isNotEmpty()) &&
+                    (parent.yEnd == imageAsLabArrays.height || neighbors.bottom.isNotEmpty())
+            ) break
+            currentNode = parent
+            parent = parent.parent ?: break
+        }
+        return neighbors
+    }
+
+    private fun collectLeftSideChildren(list: MutableSet<Node>) {
+        if (isLeaf()) {
+            list.add(this)
+        } else {
+            children[leftTopChildIndex].collectLeftSideChildren(list)
+            children[leftBottomChildIndex].collectLeftSideChildren(list)
+        }
+    }
+
+    private fun collectRightSideChildren(list: MutableSet<Node>) {
+        if (isLeaf()) {
+            list.add(this)
+        } else {
+            children[rightTopChildIndex].collectRightSideChildren(list)
+            children[rightBottomChildIndex].collectRightSideChildren(list)
+        }
+    }
+
+    private fun collectTopSideChildren(list: MutableSet<Node>) {
+        if (isLeaf()) {
+            list.add(this)
+        } else {
+            children[leftTopChildIndex].collectTopSideChildren(list)
+            children[rightTopChildIndex].collectTopSideChildren(list)
+        }
+    }
+
+    private fun collectBottomSideChildren(list: MutableSet<Node>) {
+        if (isLeaf()) {
+            list.add(this)
+        } else {
+            children[leftBottomChildIndex].collectBottomSideChildren(list)
+            children[rightBottomChildIndex].collectBottomSideChildren(list)
+        }
     }
 
 }
 
+class Neighbors {
+    val right = mutableSetOf<Node>()
+    val left = mutableSetOf<Node>()
+    val bottom = mutableSetOf<Node>()
+    val top = mutableSetOf<Node>()
+}
+
 
 class ImageAsLabArrays(bufferedImage: Image) {
-    private val width = bufferedImage.getWidth()
-    private val size = bufferedImage.getHeight() * width
+    val width = bufferedImage.getWidth()
+    val height = bufferedImage.getHeight()
+    private val size = height * width
 
     private val lValues = FloatArray(size)
     private val aValues = FloatArray(size)
     private val bValues = FloatArray(size)
 
     init {
-        for (y in 0 until bufferedImage.getHeight()) {
+        for (y in 0 until height) {
             for (x in 0 until width) {
                 val lab = rgbToLab(Color(bufferedImage.getRGB(x, y)))
                 val currentIndex = y * width + x
--- src/test/kotlin/SplitAndMergeTest.kt
+++ src/test/kotlin/SplitAndMergeTest.kt
@@ -1,5 +1,9 @@
+import adymova.nsu.grafics.core.Lab
+import adymova.nsu.grafics.panels.segmentation.ImageAsLabArrays
+import adymova.nsu.grafics.panels.segmentation.Node
 import adymova.nsu.grafics.panels.segmentation.PseudoImage
 import adymova.nsu.grafics.panels.segmentation.SplitAndMergeSegmentation
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class SplitAndMergeTest {
@@ -12,6 +16,66 @@ class SplitAndMergeTest {
                 intArrayOf(3, 3, 4, 4),
                 intArrayOf(3, 3, 4, 4)
         ))
-        SplitAndMergeSegmentation().apply(pseudoImage, 2.0) { lab1, lab2 -> if (lab1 == lab2) 0.0 else 100.0 }
+        SplitAndMergeSegmentation().apply(pseudoImage, 2.0, getTestMetrics())
+    }
+
+    private fun getTestMetrics() = { lab1: Lab, lab2: Lab -> if (lab1 == lab2) 0.0 else 100.0 }
+
+
+    @Test
+    fun testFindNeighbors() {
+        val pseudoImage = PseudoImage(arrayOf(
+                intArrayOf(7, 1, 2, 2),
+                intArrayOf(1, 1, 2, 2),
+                intArrayOf(3, 3, 4, 4),
+                intArrayOf(3, 3, 4, 4)
+        ))
+
+        val width = pseudoImage.getWidth()
+        val height = pseudoImage.getHeight()
+        val imageAsLabArrays = ImageAsLabArrays(pseudoImage)
+        val root = Node(0, 0, width, height, imageAsLabArrays, null)
+        root.pass { it.splitIfPossible(2.0, getTestMetrics()) }
+
+
+        checkNeighborsForRoot(root)
+
+        checkNeighborsNearEdge(root)
+
+        checkNeighborsInTheCenter(root)
+
+        checkNeighborsForBigArea(root)
+    }
+
+    private fun checkNeighborsForBigArea(root: Node) {
+        val neighbors = root.children[1].findNeigbors()
+        assertThat(neighbors.bottom).contains(root.children[3])
+        assertThat(neighbors.right).isEmpty()
+        assertThat(neighbors.top).isEmpty()
+        assertThat(neighbors.left).contains(root.children[0].children[1], root.children[0].children[3])
+    }
+
+    private fun checkNeighborsInTheCenter(root: Node) {
+        val neighbors = root.children[0].children[3].findNeigbors()
+        assertThat(neighbors.bottom).contains(root.children[2])
+        assertThat(neighbors.left).contains(root.children[0].children[2])
+        assertThat(neighbors.top).contains(root.children[0].children[1])
+        assertThat(neighbors.right).contains(root.children[1])
+    }
+
+    private fun checkNeighborsNearEdge(root: Node) {
+        val neighbors = root.children[0].findNeigbors()
+        assertThat(neighbors.bottom).contains(root.children[2])
+        assertThat(neighbors.left).isEmpty()
+        assertThat(neighbors.top).isEmpty()
+        assertThat(neighbors.right).contains(root.children[1])
+    }
+
+    private fun checkNeighborsForRoot(root: Node) {
+        val neighbors = root.findNeigbors()
+        assertThat(neighbors.bottom).isEmpty()
+        assertThat(neighbors.left).isEmpty()
+        assertThat(neighbors.top).isEmpty()
+        assertThat(neighbors.right).isEmpty()
     }
 }
